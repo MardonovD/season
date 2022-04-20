1c6cda6e237df4517730a9ac91c38cdab056d7fe
MardonovD <mardonovdoston69@gmail.com>
2022-04-20 11:42:08 +0500

    season created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+// const App = () => {
+//   return <div>Hello World</div>;
+// };
+
+class App extends React.Component {
+
+  render() {
+   return <div>Hello World</div>;
+  }
+
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+
+
+
+

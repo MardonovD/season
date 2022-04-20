ed13fcbb70f824ee4d3362d248df82d1115f22c4
MardonovD <mardonovdoston69@gmail.com>
2022-04-20 11:54:22 +0500

    navigator

--- src/index.js
+++ src/index.js
@@ -3,21 +3,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // const App = () => {
-//   return <div>Hello World</div>;
+//   navigator.geolocation.getCurrentPosition(
+//     function (pos) {
+//       console.log(pos);
+//     },
+//     function (err) {
+//       console.log(err);
+//     }
+//   );
+//   return <div>Sani joylashuving {pos}</div>;
 // };
 
 class App extends React.Component {
-
-  render() {
-   return <div>Hello World</div>;
+  constructor(props) {
+    super(props);
+    this.state;
   }
-
+  render() {
+    navigator.geolocation.getCurrentPosition(
+      function (pos) {
+        console.log(pos);
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+    return <div>Hello World</div>;
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-
-
-
-
-
